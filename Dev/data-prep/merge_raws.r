0e807006589a53d3c5e5df2add1e2aeee04b1833
Goodernews <tbogoodnews@gmail.com>
2021-04-17 14:29:44 -0700

    Updated

--- Dev/data-prep/merge_raws.r
+++ Dev/data-prep/merge_raws.r
@@ -7,19 +7,28 @@ files_listed <- list.files(path)
 print(files_listed)
 
 combined <- read_csv(paste(path, files_listed[1], sep = "/")) %>%
-  mutate(created_utc = as.character(created_utc),
-         score = as.character(score),
+  mutate(created_utc = as.integer(created_utc),
+         score = as.integer(score),
          num_comments = as.character(num_comments))
 
+if("upvote_ratio" %in% names(combined)){
+  combined <- combined %>%
+    subset(select=-(upvote_ratio))
+}
 
 
 files_listed <-files_listed[-1] #removes the first file
 
 for (file in files_listed){
   opened_file <- read_csv(paste(path, file, sep = "/")) %>%
-    mutate(created_utc = as.character(created_utc),
-           score = as.character(score),
-           num_comments = as.character(num_comments))
+    mutate(created_utc = as.integer(created_utc),
+           score = as.integer(score),
+           num_comments = as.integer(num_comments))
+  
+  if("upvote_ratio" %in% names(opened_file)){
+    opened_file <- opened_file %>%
+      subset(select=-(upvote_ratio))
+  }
   combined <- bind_rows(combined, opened_file)
 }
 
