332705ba23ca40d90a6f0cd3a33f6335ea0ef9d4
Goodernews <tbogoodnews@gmail.com>
2021-04-23 17:53:48 -0700

    Updated

--- Dev/data-prep/emoji_extract.r
+++ Dev/data-prep/emoji_extract.r
@@ -1,9 +1,36 @@
 #devtools::install_github("hadley/emo")
+
 library(tidyverse)
 library(emo)
+library(tidytext)
+library(ggplot2)
 
-wsb <- read_csv("data/wsb_dd_submissions.csv") %>%
-  mutate(emojis = unique(ji_extract_all(title)))
+
+
+sentiments <- get_sentiments("afinn")
+calc_sentiment <- function(string){
+  split <- unlist(strsplit(string, "[[:punct:] ]"))
+  indexed <- match(split, sentiments$word)
+  vals <- sentiments$value[indexed]
+  return(sum(vals, na.rm=TRUE))
+}
+
+wsb <- read_csv("data/wsb_dd_submissions.csv")
+
+wsb$title_sentiment <- map(wsb$title, calc_sentiment)%>%
+  unlist()
+
+wsb$post_sentiment <- map(wsb$selftext, calc_sentiment)%>%
+  unlist()
+
+
+wsb %>%
+  ggplot(aes(post_sentiment)) +
+  geom_histogram()
+
+
+wsb <- wsb %>%
+  mutate(emojis = ji_extract_all(title))
 
 ji_extract_all(wsb$title[275])
 
