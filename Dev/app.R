5ff9013284ba33ce598b766bca914ca8e920cd62
ahnsb5117 <ahnsb5117@gmail.com>
2021-05-04 12:10:31 -0700

    kmean shiny

--- Dev/app.R
+++ Dev/app.R
@@ -5,13 +5,8 @@ library(india.air)
 library(shiny) 
 library(lubridate)
 
-india_air <- india_air %>% 
-  mutate(date = ymd(date)) %>%
-  drop_na(date)
+wsb_dd_submissions <- read_csv("../WSB-viz/www/wsb_dd_submissions.csv")
 
-glimpse(india_air)
-
-india_air <- india_air[india_air$city != "Mumbai", ]
 
 # User interface
 ui <- fluidPage(
@@ -26,57 +21,52 @@ ui <- fluidPage(
                      end = "2020-07-01"),
       numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
     ),
-    mainPanel(
-      tabsetPanel(type = "tabs",
-                  tabPanel("Plot", plotOutput(outputId = "graph")),
-                  tabPanel("Summary", tags$p("The data shows the pollution level of Major Cities in India. You can explore the different factors of pollutions like PM2.5, NO, NO2 and CO."),
-                           dataTableOutput(outputId = "table")),
-                  tabPanel("Reference", 
-                           tags$p("The data were obtained from the",
-                                  tags$code("india.air"), "library in R."))
       )
     )
-  )
-)
+
 
 
 
 # Server function
-# Server function
-server <- function(input, output, session){
+server <- function(input, output, session) {
   
-  updateSelectizeInput(session, 'names', 
-                       choices = unique(india_air$city), 
-                       server = TRUE)
-  
-  dat_names <- reactive({ 
-    india_air %>%
-      filter(city %in% c(unlist(str_split(input$names, " "))),
-             date >= input$year_range[1],
-             date <= input$year_range[2]) 
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    iris[, c(input$sentiment, input$count)]
   })
   
-  output$graph <- renderPlot({
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  
+  userSelectDate2 <- wsb_dd_submissions %>%
+    filter(date >= as.Date("2021-01-01") & date <= as.Date("2021-01-05")) %>%
+    filter(post_sentiment != 0)
+  sentiment <- userSelectDate2 %>%
+    group_by(ticker) %>%
+    summarise(sentiment = sum(post_sentiment)) %>% 
+    drop_na()
+  mentionedTimes <- userSelectDate2 %>%
+    group_by(date) %>%
+    count(ticker) %>% 
+    group_by(ticker) %>%
+    summarise(count = sum(n))
+  kmeanData <- merge(sentiment, mentionedTimes, by = "ticker") 
+  
+  
+  
+  output$plot1 <- renderPlot({
+    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
     
-    ggplot(data = dat_names(), 
-           mapping = aes(x = date, y = .data[[input$variable]],
-                         color = city)) +
-      geom_line()
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
   })
   
-  
-  dat_names_agg <- reactive({ 
-    dat_names() %>%
-      group_by(city) %>%
-      arrange(city)
-  })
-  
-  output$table <-  renderDataTable({
-    datatable(dat_names_agg(), 
-              options = list(paging = FALSE,
-                             searching = FALSE,
-                             orderClasses = TRUE))
-  })
 }
 
 
