125751eea7f36f00c1f20b56be5d851d479601bc
ahnsb5117 <ahnsb5117@gmail.com>
2021-05-04 12:24:49 -0700

    kmean app

--- Dev/app.R
+++ Dev/app.R
@@ -5,8 +5,25 @@ library(india.air)
 library(shiny) 
 library(lubridate)
 
-wsb_dd_submissions <- read_csv("../WSB-viz/www/wsb_dd_submissions.csv")
+wsb_dd_submissions <- read_csv("../WSB-viz/www/wsb_dd_submissions.csv") %>% 
+  mutate(ticker=strsplit(title_stocks, " ")) %>% 
+  unnest(ticker) %>% 
+  drop_na(ticker)
 
+
+vars <- setdiff(names(iris), "Species")
+
+pageWithSidebar(
+  headerPanel('Iris k-means clustering'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', vars),
+    selectInput('ycol', 'Y Variable', vars, selected = vars[[2]]),
+    numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
+  ),
+  mainPanel(
+    plotOutput('plot1')
+  )
+)
 
 # User interface
 ui <- fluidPage(
@@ -21,7 +38,7 @@ ui <- fluidPage(
                      end = "2020-07-01"),
       numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
     ),
-      )
+      
     )
 
 
@@ -39,22 +56,22 @@ server <- function(input, output, session) {
     kmeans(selectedData(), input$clusters)
   })
   
-  
   userSelectDate2 <- wsb_dd_submissions %>%
     filter(date >= as.Date("2021-01-01") & date <= as.Date("2021-01-05")) %>%
     filter(post_sentiment != 0)
+  
   sentiment <- userSelectDate2 %>%
     group_by(ticker) %>%
     summarise(sentiment = sum(post_sentiment)) %>% 
     drop_na()
+  
   mentionedTimes <- userSelectDate2 %>%
     group_by(date) %>%
     count(ticker) %>% 
     group_by(ticker) %>%
     summarise(count = sum(n))
+  
   kmeanData <- merge(sentiment, mentionedTimes, by = "ticker") 
-  
-  
   
   output$plot1 <- renderPlot({
     palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
